3bb608f5012045775315262b9d516741bc8a7a14
JpHendrix51 <jonathan.e.perez@gmail.com>
2019-08-12 23:49:08 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -2,16 +2,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+class Queue:
 
-    def __repr__(self):
-        return '<Person %r>' % self.username
+    def __init__(self, mode):
+        self._queue = []
+        # depending on the _mode, the queue has to behave like a FIFO or LIFO
+        self._mode = mode
 
-    def serialize(self):
-        return {
-            "username": self.username,
-            "email": self.email
-        }
+    def enqueue(self, item):
+        self._queue.append(item)
+        return True
+
+    def dequeue(self):
+        if self._mode == "FIFO":
+             return self._queue.pop(0)
+        else:
+            return self._queue.pop(-1)
+
+    def get_queue(self):
+        return self._queue
+    def size(self):
+        return len(self._queue)
+
+
